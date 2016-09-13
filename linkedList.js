ea23f499c3a82a9f0f04bf6150e229383e63a4f9
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-12 14:29:40 -1000

    initial commit: added method stubs, passed 8 tests

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,32 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  module = {};
 
+  module.getHead = function() {
+
+  };
+
+  module.getTail = function() {
+
+  };
+
+  //adds a value to the end of a list
+  module.add = function(value) {
+
+  };
+
+  module.remove = function(index) {
+
+  };
+
+  module.get = function(index) {
+
+  };
+
+  module.insert = function(value, index) {
+
+  };
+
+  return module;
 }
