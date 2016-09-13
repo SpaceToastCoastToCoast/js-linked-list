439551d0264b5545b33c4f4094d636b2ec2e13ec
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-13 12:05:19 -1000

    implemented basic visualization

--- linkedListVisualizer.js
+++ linkedListVisualizer.js
@@ -1,19 +1,35 @@
 const linkedListVisualizer = function() {
   let vis = {};
   let list = linkedListGenerator();
+
+  let nodeValue = document.getElementById('nodeValue');
+  let controlsDiv = document.getElementById('controls');
+  let listDiv = document.getElementById('list');
+
   let addNodeButton = document.createElement('button');
-  addNodeButton.innerHTML = "Add Node";
-  document.body.insertBefore(addNodeButton);
+  addNodeButton.id = 'addNodeButton';
+  addNodeButton.innerHTML = 'Add Node';
+  controlsDiv.appendChild(addNodeButton);
+
+  vis.addNode = function() {
+    list.add(nodeValue.value);
+    console.log(list.getTail());
+    vis.updateDisplay();
+  };
+
+  addNodeButton.addEventListener('click', vis.addNode);
 
   vis.updateDisplay = function() {
+    listDiv.innerHTML = "";
     for(let i = 0; i < list.getLength(); i++) {
       let nodeDiv = document.createElement('div');
       let nodeContent = document.createTextNode(list.get(i).value);
       nodeDiv.appendChild(nodeContent);
-      document.body.insertBefore(nodeDiv);
+      listDiv.appendChild(nodeDiv);
     }
   };
 
+  return vis;
 };
 
 linkedListVisualizer();
