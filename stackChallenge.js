770d5cc32f64c6920e1c8e1fe0b250a3ef42286c
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-14 09:52:40 -1000

    edits for convenience and clarity

--- stackChallenge.js
+++ stackChallenge.js
@@ -3,15 +3,26 @@ const stackChallenge = function() {
   let list = linkedListGenerator();
   let listDiv = document.getElementById('list');
   let textArea = document.getElementById('textBox');
+  let infoBox = document.getElementById('infoBox');
   let dumpButton = document.getElementById('dumpButton');
   let saveButton = document.getElementById('saveButton');
 
   stack.saveNode = function() {
     list.add(textArea.value);
-    textArea.value = 'Added node. Specify next node';
+    textArea.value = '';
+    infoBox.innerHTML = 'Added node. Specify next node';
   };
 
   saveButton.addEventListener('click', stack.saveNode);
+
+  stack.listItemToString = function(index) {
+    if(list.get(index) !== false) {
+      result = "Value: " + list.get(index).value +
+      "<br />Next item: " + list.get(index).next.value;
+
+      return result;
+    }
+  };
 
   stack.dump = function() {
     //clear our list each time we initialize dump
@@ -19,8 +30,10 @@ const stackChallenge = function() {
 
     //dump our list to the page from tail to head
     for(let i = list.getLength() - 1; i >= 0; i--) {
-      listDiv.innerHTML += "<p>" + list.get(i) + "</p>";
+      listDiv.innerHTML += "<p>" + stack.listItemToString(i) + "</p>";
     }
+
+    infoBox.innerHTML = 'Stack dumped, most recent node at top';
   };
 
   dumpButton.addEventListener('click', stack.dump);
